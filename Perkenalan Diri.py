f0d77a0d68e439a559967ea7be4b22b842115e06
DinaSeptyP <94428761+DinaSeptyP@users.noreply.github.com>
2021-11-19 22:50:05 +0700

    Update Perkenalan Diri.py

--- Perkenalan Diri.py
+++ Perkenalan Diri.py
@@ -5,9 +5,9 @@ umur = 18
 prodi = "Teknologi Informasi"
 asalUniv = "Universitas Jember"
 
-print("-"*90)
+print("-" * 90)
 print("|", "Hai semua, nama saya", namaLengkap + ",", "biasa dipanggil Dina.", "\t", "\t", "\t", "|")
 print("|", "Saya berumur", umur, "tahun" + ",", "asal saya adalah dari", asalKota, "\t", "\t", "\t", "\t", "|")
 print("|", "Kali ini saya sedang menempuh studi di prodi", prodi + ",", asalUniv, "\t", "|")
 print("|", "Nice to meet you~", "\t", "\t", "\t", "\t", "\t", "\t", "\t", "\t", "\t", "|")
-print("-"*90)
+print("-" * 90)
