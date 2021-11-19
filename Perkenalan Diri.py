7a7710cc14017ee3c315000232266035f66b5367
DinaSeptyP <94428761+DinaSeptyP@users.noreply.github.com>
2021-11-19 22:49:06 +0700

    Add files via upload

---
+++ Perkenalan Diri.py
@@ -0,0 +1,13 @@
+namaLengkap = "Dina Septy Puspayani" 
+nomorNIM = 212410102046
+asalKota = "Bojonegoro"
+umur = 18
+prodi = "Teknologi Informasi"
+asalUniv = "Universitas Jember"
+
+print("-"*90)
+print("|", "Hai semua, nama saya", namaLengkap + ",", "biasa dipanggil Dina.", "\t", "\t", "\t", "|")
+print("|", "Saya berumur", umur, "tahun" + ",", "asal saya adalah dari", asalKota, "\t", "\t", "\t", "\t", "|")
+print("|", "Kali ini saya sedang menempuh studi di prodi", prodi + ",", asalUniv, "\t", "|")
+print("|", "Nice to meet you~", "\t", "\t", "\t", "\t", "\t", "\t", "\t", "\t", "\t", "|")
+print("-"*90)
